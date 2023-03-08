efc138320a6ab97b874156ef64fa635725b928ad
MATEUS RODRIGUES DA SILVA <mateus.rsilva30@senacsp.edu.br>
2023-03-07 22:06:18 -0300

    while

--- UC9_SENAC/Program.cs
+++ UC9_SENAC/Program.cs
@@ -1,30 +1,80 @@
 ﻿string[,] faltas = new string[2, 3];
-string opcao;
+int opcao = 0; ;
 int l, c;
 double presenca;
 double hora = 0;
+string pesquisar = "";
+string busca = "";
+string voltar = "sim";
 
 l = 0;
 c = 0;
 
-for (l = 0; l <= 1; l++)
-{
-    Console.WriteLine("informe o numero da UC:");
-    faltas[l, c++] = Console.ReadLine();
-    Console.WriteLine("Informe a descrição da UC:");
-    faltas[l, c++] = Console.ReadLine();
-    Console.WriteLine("Informe a carga horaria da UC:");
-    faltas[l, c++] = Console.ReadLine();
+while (voltar == "sim" || voltar == "SIM") {
 
-    c = 0;
-}
+    Console.WriteLine("Digite 1 para cadastrar");
+    Console.WriteLine("Digite 2 para pesquisar a quantidade de faltas");
+    opcao = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Qual a duração de sua aula diária:");
-hora = double.Parse(Console.ReadLine());
+    switch (opcao)
+    {
 
-for (l = 0; l <=1; l++)
-{
-    presenca = (double.Parse(faltas[l, 2]) * 0.25) / hora;
-    Console.WriteLine("A quantidade de faltas que voce pode ter é: " + presenca);
+        case 1:
+            for (l = 0; l <= 1; l++)
+            {
+                Console.WriteLine("informe o numero da UC:");
+                faltas[l, c++] = Console.ReadLine();
+                Console.WriteLine("Informe a descrição da UC:");
+                faltas[l, c++] = Console.ReadLine();
+                Console.WriteLine("Informe a carga horaria da UC:");
+                faltas[l, c++] = Console.ReadLine();
 
+                c = 0;
+            }
+
+            break;
+
+        case 2:
+            Console.WriteLine("Qual a duração de sua aula diária:");
+            hora = double.Parse(Console.ReadLine());
+
+            for (l = 0; l <= 1; l++)
+            {
+                pesquisar = "SIM";
+                while (pesquisar == "SIM" || pesquisar == "sim")
+                {
+
+                    Console.WriteLine("Qual a duração de sua aula diária:");
+                    hora = double.Parse(Console.ReadLine());
+
+                    presenca = (double.Parse(faltas[l, 2]) * 0.25) / hora;                   
+
+                    Console.WriteLine("UC que deseja pesquisar");
+                    busca = Console.ReadLine();
+
+                    for (l = 0; l <= 1; l++)
+                    {
+                        if (busca == faltas[l, 0])
+                        {
+                            Console.WriteLine("Na " + faltas[l, 0] + "" + faltas[l, 1] + " A quantidade de faltas que voce pode ter é: " + presenca);
+
+                        }
+                    }
+
+
+                    Console.WriteLine("Deseja pesquisar a quantidade de faltas em outra UC");
+                    pesquisar = Console.ReadLine();
+                }
+            }
+            break;
+
+        default:
+
+            break;
+
+
+    }
+    
+    Console.WriteLine("Deseja voltar para CADASTRAR ou PESQUISAR");
+    voltar = Console.ReadLine();
 }
