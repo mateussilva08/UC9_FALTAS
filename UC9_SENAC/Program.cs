18de3a766edbbc419e79ed1aa7971e5b6cd41021
MATEUS RODRIGUES DA SILVA <mateus.rsilva30@senacsp.edu.br>
2023-03-07 21:30:18 -0300

    calculo

--- UC9_SENAC/Program.cs
+++ UC9_SENAC/Program.cs
@@ -1,6 +1,24 @@
 ﻿string[,] faltas = new string[2, 3];
 string opcao;
 int l, c;
+double presenca;
 
 l = 0;
 c = 0;
+
+for (l = 0; l <= 2; l++)
+{
+    Console.WriteLine("informe o numero da UC:");
+    faltas[l, c++] = Console.ReadLine();
+    Console.WriteLine("Informe a descrição da UC:");
+    faltas[l, c++] = Console.ReadLine();
+    Console.WriteLine("Informe a carga horaria da UC:");
+    faltas[l, c++] = Console.ReadLine();
+
+    c = 0;
+}
+
+for (l = 0; l <=2; l++)
+{
+    presenca = 
+}
