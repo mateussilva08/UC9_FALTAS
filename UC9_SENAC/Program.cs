b5dd2359e400e05975e2de0b33bc113882c7afd2
MATEUS RODRIGUES DA SILVA <mateus.rsilva30@senacsp.edu.br>
2023-03-07 21:52:14 -0300

    switch

--- UC9_SENAC/Program.cs
+++ UC9_SENAC/Program.cs
@@ -2,11 +2,12 @@
 string opcao;
 int l, c;
 double presenca;
+double hora = 0;
 
 l = 0;
 c = 0;
 
-for (l = 0; l <= 2; l++)
+for (l = 0; l <= 1; l++)
 {
     Console.WriteLine("informe o numero da UC:");
     faltas[l, c++] = Console.ReadLine();
@@ -18,7 +19,12 @@ for (l = 0; l <= 2; l++)
     c = 0;
 }
 
-for (l = 0; l <=2; l++)
+Console.WriteLine("Qual a duração de sua aula diária:");
+hora = double.Parse(Console.ReadLine());
+
+for (l = 0; l <=1; l++)
 {
-    presenca = 
+    presenca = (double.Parse(faltas[l, 2]) * 0.25) / hora;
+    Console.WriteLine("A quantidade de faltas que voce pode ter é: " + presenca);
+
 }
